e57cca8c5fbd3275f081a93dfa8960ebba55efef
VTN <37680210+VTNSCC@users.noreply.github.com>
2021-06-22 05:02:16 -0300

    Add files via upload

---
+++ scripts/cpf.js
@@ -0,0 +1,35 @@
+function verificaCPF(cpf) {
+	var dv1 = calculaDV1(cpf);
+	var dv2 = calculaDV2(cpf);
+	if(dv1 == cpf[9] && dv2==cpf[10]){
+		return true;
+	}else{
+		return false;
+	}
+}
+function calculaDV1(cpf) {
+	var pesos = [10,9,8,7,6,5,4,3,2];
+	var soma = 0;
+	for(var i = 0; i<9;i++){
+		soma = soma + (pesos[i]* Number(cpf[i]) );
+	}
+	var resto = soma%11;
+	if(resto < 2){
+		return 0;
+	}else{
+		return 11 - resto;
+	}
+}
+function calculaDV2(cpf) {
+	var pesos = [11,10,9,8,7,6,5,4,3,2];
+	var soma = 0;
+	for(var i = 0; i<10;i++){
+		soma = soma + (pesos[i]* Number(cpf[i]) );
+	}
+	var resto = soma%11;
+	if(resto < 2){
+		return 0;
+	}else{
+		return 11 - resto;
+	}
+}
